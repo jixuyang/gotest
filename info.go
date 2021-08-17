f5afecb52e8cf4c9bb46cd10564309fd9bc5bf12
jason.yang <jason.yang@splashtop.com>
2021-08-17 16:17:34 +0800

    Update to v2.1.1

--- info.go
+++ info.go
@@ -9,23 +9,26 @@ import (
 
 const (
 	module = "github.com/jixuyang/gotest"
-	tag    = "v2.1.0"
+	tag    = "v2.1.1"
 )
 
 func init() {
 	log.Infof("[%s] [%s] init", tag, module)
 }
 
+// GetInfo get info
 func GetInfo() (string, error) {
 	msg := fmt.Sprintf("[%s] [%s] GetInfo: return module info", tag, module)
 	return msg, nil
 }
 
+// GetLicense get license
 func GetLicense() (string, error) {
 	msg := fmt.Sprintf("[%s] [%s] GetLicense: return module license", tag, module)
 	return msg, nil
 }
 
+// GetConfig get config
 func GetConfig() (string, error) {
 	msg, err := api.GetConfig()
 	return msg, err
